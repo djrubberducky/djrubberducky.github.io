690e7bfb0f3af2d76bf4cff36ee25d2b33706bb1
Daria Bilous <daria.bilous@icloud.com>
2018-05-13 16:39:58 +0300

    Initialize project

---
+++ index2.php
@@ -0,0 +1,39 @@
+<?php 
+
+$a = 100;
+$b = 15;
+
+$c = array(1, 2, 3, 4, 5);
+
+echo $a;
+echo '<hr>';
+echo $b;
+echo $c[2]
+$weather = 'snow';
+if( $weather == 'snow' ) {
+   echo 'It is quiet cold oitside';
+}
+if( $weather == 'rain' ) {
+   echo 'Do not forget your umbrella';
+}
+if( $weather == 'sunny' )
+ {
+   echo 'Go for a walk!';
+}
+
+$age = 20;
+//пропуск в клуб 24
+if ( $age < 24 ) 
+{
+	echo 'you are too young for club';
+}
+
+if ( $age >= 24 )
+{
+	echo 'Enter';
+} else {
+
+	echo 'unique nuber';
+}
+
+ ?>
